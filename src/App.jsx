3cf269298756eeb9870594843ab3af4ed63fc2e5
suraj suryavanshi <93034090+srj393@users.noreply.github.com>
2026-02-05 10:46:25 +0530

    test5

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { BrowserRouter as Router , Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
-import Contact from "./pages/contact";
+// import Contact from "./pages/contact";
 import Navbar from "./components/Navbar/Navbar"
 
 
