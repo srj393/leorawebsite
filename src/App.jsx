4765abf2eff19a4ae2e94a7785a4a465fb3b7717
suraj suryavanshi <93034090+srj393@users.noreply.github.com>
2026-02-05 10:43:26 +0530

    test4

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
 <Navbar />
 <Routes>
         <Route path="/" element={<Home />} / > 
-        <Route path="/contact" element={<Contact />} / >
+       {/* <Route path="/contact" element={<Contact />} / > */}
        
        
       </Routes>
